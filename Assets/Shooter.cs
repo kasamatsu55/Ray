cab45318bc7b93e3a3d81e0523977caf2effc663
kasamatsu <kasamatsu.shoji@gmail.com>
2022-02-12 18:46:06 +0900

    first commit

---
+++ Assets/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray,out hit,100f))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
